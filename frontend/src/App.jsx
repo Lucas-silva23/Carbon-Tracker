697096452eb274f4d08768c0019cafb2e01a4041
Lucas-silva23 <lucasarm.silva@gmail.com>
2025-10-27 18:15:13 -0300

    add: logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
 import HomePage from './pages/HomePage';
-import AuthPage from './pages/AuthPage'; // 1. IMPORTAR A NOVA PÁGINA
+import AuthPage from './pages/AuthPage'; 
 import DashboardPage from './pages/DashboardPage';
 import ProtectedRoute from './components/ProtectedRoute';
-
-// 2. REMOVA OS IMPORTS de LoginPage e RegisterPage
 
 function App() {
   return (
     <Routes>
-      {/* Rotas Públicas */}
       <Route path="/" element={<HomePage />} />
-      <Route path="/auth" element={<AuthPage />} /> {/* 3. ADICIONAR NOVA ROTA */}
+      <Route path="/auth" element={<AuthPage />} />
       
-      {/* 4. REMOVER as rotas /login e /register */}
-      
-      {/* Rota Protegida */}
       <Route 
         path="/dashboard" 
         element={
